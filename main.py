8bbbcecc5245c2bedab1c9defd1d481ce093b445
CrowMother <94501083+CrowMother@users.noreply.github.com>
2025-04-21 10:45:12 -0500

    basic main with plan for building out the loop

---
+++ main.py
@@ -0,0 +1,31 @@
+# reports bot main
+
+import os
+import logging
+import Bot_App as bot
+import dotenv
+from Bot_App import gsheet, util, schwab, data
+
+def main():
+    logging.info("Starting Reports Bot ...")
+
+    # Load environment variables
+
+    # Create schwab client
+
+    # Create gsheet client
+
+
+    # loop
+
+    # check time to run
+    # if not time to run, sleep for 5 minutes and jump to loop
+
+    # get Schwab data
+
+    # update gsheet with Schwab data
+
+    # jump to loop
+
+if __name__ == "__main__":
+    main()
