3defa51b7408851b8f13fe541fbc3591fbbdd59c
CrowMother <94501083+CrowMother@users.noreply.github.com>
2025-04-22 21:35:19 -0500

    got posting to gsheet to work with bot_framework

--- main.py
+++ main.py
@@ -4,26 +4,45 @@ import os
 import logging
 import Bot_App as bot
 import dotenv
-from Bot_App import gsheet, util, schwab, data
+from Bot_App import gsheet, util, schwab, data, SQL
+
+
+FILTER = "FILLED"
+TIME_DELTA=12
 
 def main():
+    SQL.initialize_db()
+
     logging.info("Starting Reports Bot ...")
-
-    # Load environment variables
-
     # Create schwab client
-
+    client = bot.Schwab_client(
+                bot.util.get_secret("SCHWAB_APP_KEY", "config/.env"),
+                bot.util.get_secret("SCHWAB_APP_SECRET", "config/.env")
+            )
+    print("Schwab client initialized")
     # Create gsheet client
-
-
+    gclient = gsheet.connect_gsheets_account(util.get_secret("GSHEETS_CREDENTIALS", "config/.env"))
+    worksheet = gsheet.connect_to_sheet(gclient, util.get_secret("GSHEETS_SHEET_ID", "config/.env"), util.get_secret("GSHEETS_PAGE_NAME", "config/.env"))
     # loop
 
     # check time to run
     # if not time to run, sleep for 5 minutes and jump to loop
 
-    # get Schwab data
+    # Create new header row for this date
+    header_location = gsheet.get_next_empty_row(worksheet, 2)
+    gsheet.copy_headers(worksheet, header_location)
 
+    # get Schwab data
+    response = client.get_account_positions(FILTER, TIME_DELTA)
+    print("Schwab data retrieved")
+    # store and process Schwab data
+    data.store_orders(response)
+    unposted_orders = data.get_unposted_orders()
     # update gsheet with Schwab data
+    for order in response:
+        row_data = gsheet.format_data(order)
+        gsheet.write_row_at_next_empty_row(worksheet, row_data)
+        #data.mark_as_posted(order[0])
 
     # jump to loop
 
